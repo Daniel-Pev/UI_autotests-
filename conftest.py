c9ce036ab095f132cd4620553362b96209822ce7
Daniil Pelevin <walkingdisaster322@gmail.com>
2024-12-05 19:32:13 +0300

    adding main_page tests

--- conftest.py
+++ conftest.py
@@ -3,18 +3,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-
 @pytest.fixture(scope="function")
 def browser():
     options = Options()
     options.add_argument("--lang=en")
     options.add_argument("--no-sandbox")
-    options.add_argument("start-maximized") # open Browser in maximized mode
-    options.add_argument("--disable-infobars") # disabling infobars
-    options.add_argument("--disable-extensions") # disabling extensions
-    options.add_argument("--disable-gpu") #  applicable to windows os only
-    options.add_argument("--disable-dev-shm-usage") # overcome limited resource problems
+    options.add_argument("start-maximized")  # open Browser in maximized mode
+    options.add_argument("--disable-infobars")  # disabling infobars
+    options.add_argument("--disable-extensions")  # disabling extensions
+    options.add_argument("--disable-gpu")  # applicable to windows os only
+    options.add_argument("--disable-dev-shm-usage")  # overcome limited resource problems
     options.add_argument("--disable-search-engine-choice-screen")
+
     driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
